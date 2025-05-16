6aac2a2a64643a29adc389ef6128acda2c0d11c1
EmilLidir <lidiremil@gmx.de>
2025-05-17 00:56:51 +0200

    Update rct.py

--- rct.py
+++ rct.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.common.exceptions import WebDriverException # <<< ADDED IMPORT
+from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import TimeoutException as SeleniumTimeoutException
 import websocket
 import time
